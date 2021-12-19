6261ab0dfbaa76684d1d3ed64dc2d15367b9b29d
BuiThanhTung <tung.bt184324@sis.hust.edu.vn>
2021-12-19 22:10:34 +0700

    add report for lab10

--- code/java/src/src/main/java/entity/station/StationList.java
+++ code/java/src/src/main/java/entity/station/StationList.java
@@ -7,18 +7,33 @@ import java.util.List;
 import entity.station.Station;
 
 public class StationList {
-    private List<Station> lstStation;
 
-    public StationList() {
-        this.lstStation = new ArrayList<>();
+  private List<Station> lstStation;
+  private static StationList StationListInstance;
+
+
+  public StationList() {
+    this.lstStation = new ArrayList<>();
+  }
+
+  public StationList(List<Station> lstStation) {
+    this.lstStation = lstStation;
+  }
+
+  public void addStation(Station station) {
+
+  }
+
+  public static StationList getStationListInstance() {
+    if (StationListInstance == null) {
+      StationListInstance = new StationList();
     }
+    return StationListInstance;
+  }
 
-    public StationList(List<Station> lstStation) {
-        this.lstStation = lstStation;
-    }
+  public List<Station> getStationList() {
+    return lstStation;
+  }
 
-    public void addStation(Station station) {
-
-    }
 
 }
